e9ecc1d97cdad5d34be2b54add9717af52e6b011
LanceDH <kiep_out@hotmail.com>
2014-12-29 18:08:53 +0100

    More items
    Added items for every slot and attribute combination

--- ProjectGame/src/UI/LogingPanel.java
+++ ProjectGame/src/UI/LogingPanel.java
@@ -173,7 +173,7 @@ public class LogingPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_btn_LoginActionPerformed
 
     private void btn_ExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_ExitActionPerformed
-        _parent.dispose();
+        System.exit(0);
     }//GEN-LAST:event_btn_ExitActionPerformed
 
 
