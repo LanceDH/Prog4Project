b12e5b225c03511cb4110120156c49f08796963a
LanceDH <kiep_out@hotmail.com>
2015-01-04 15:40:05 +0100

    Better Validators

--- ProjectGame/src/Services/Validators.java
+++ ProjectGame/src/Services/Validators.java
@@ -20,6 +20,10 @@ public class Validators {
     
     public static boolean IsValidAccountName(String name) throws UIException{
         
+        if (!name.matches("^[a-zA-Z]*$")) {
+            throw new UIException("Name can only contain letters.");
+        }
+        
         if(name.length() < ACCNAME_MINLENGTH){
             throw new UIException("Name has to be at least "+ ACCNAME_MINLENGTH +" letters.");
         }
@@ -28,10 +32,16 @@ public class Validators {
             throw new UIException("Name must be less than "+ ACCNAME_MAXLENGTH +" letters.");
         }
         
+        
+        
         return true;
     }
     
     public static boolean IsValidPassword(String pass) throws UIException{
+        
+        if (!pass.matches("^[a-zA-Z0-9]*$")) {
+            throw new UIException("Password can only contain letters and numbers.");
+        }
         
         if(pass.length() < PASSWORD_MINLENGTH){
             throw new UIException("Password has to be at least "+ ACCNAME_MINLENGTH +" letters.");
@@ -45,6 +55,15 @@ public class Validators {
     }
     
     public static boolean IsValidCharacterName(String name) throws UIException{
+        
+        if (!name.matches("^[a-zA-Z ]*$")) {
+            throw new UIException("Name can only contain letters and spaces.");
+        }
+        
+        if (!name.matches("^[[\\p{Alpha}]]+( [[\\p{Alpha}}]]+)*$")) {
+            throw new UIException("Name has invalid use of spaces.");
+        }
+        
         if(name.length() < CHARACTER_MINLENGTH){
             throw new UIException("Name has to be at least "+ CHARACTER_MINLENGTH +" letters.");
         }
